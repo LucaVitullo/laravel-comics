937be208d2fb7b986b32fb429d1f52ce862622fe
Lucavit9 <95365833+Lucavit9@users.noreply.github.com>
2022-05-18 18:50:43 +0200

    layout sistemato, generate dinamicamente le immagini(fumetti)

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -2,17 +2,27 @@
 
 @section('main')
     <main>
-        <div class="container">
-            <div class="contents">
-                @foreach ($comics as $comic)
-                    <div class="box">
-
-                        <img src="{{ $comic['thumb'] }}" alt="img">
-                        <p>{{ $comic['title'] }}</p>
-
-                    </div>
-                @endforeach
+        <div class="contents">
+            <div class="container-bg_main-top">
+                <div class="label-main-top">CURRENT SERIES</div>
             </div>
+            <div class="container-bg-main-bottom">
+                <div class="content-dc-series">
+                    @foreach ($comics as $comic)
+                        <div class="card-fumetto">
+
+                            <img src="{{ $comic['thumb'] }}" alt="img">
+                            <p>{{ $comic['series'] }}</p>
+
+                        </div>
+                    @endforeach
+
+                </div>
+                <div class="container-button">
+                    <button>LOAD MORE</button>
+                </div>
+            </div>
+
         </div>
     </main>
 @endsection
