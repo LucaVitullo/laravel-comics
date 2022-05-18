a06dac5c774775046abdc8136d6a3f12179a8f51
Lucavit9 <95365833+Lucavit9@users.noreply.github.com>
2022-05-18 15:01:43 +0200

    inclusi header e footer tramite partials in app.blade

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="it">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('title')</title>
+</head>
+
+<body>
+    @include('partials.header')
+
+
+
+
+
+
+
+
+    @include('partials.footer')
+</body>
+
+</html>
