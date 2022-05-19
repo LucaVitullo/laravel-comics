2b641d5fbee1de92494ae1899858b10902fb9823
Lucavit9 <95365833+Lucavit9@users.noreply.github.com>
2022-05-19 18:58:21 +0200

    exercise completed

--- resources/views/partials/singleCard.blade.php
+++ resources/views/partials/singleCard.blade.php
@@ -7,8 +7,11 @@
     </div>
     <div class="bg-blue">
         <img src="{{ $comics['thumb'] }}" alt="{{ $comics['series'] }}">
+        <a href="/">
+            <button class="view-gallery">VIEW GALLERY</button>
+        </a>
     </div>
-    <div class="section-main">
+    <div class="section-main-1">
         <div class="container-infoCard">
             <h2>
                 {{ $comics['title'] }}
@@ -29,7 +32,61 @@
             </div>
         </div>
         <div class="advertisement">
+            <span>ADVERTISEMENT</span>
             <img src="images/advertise.jpg" alt="">
+        </div>
+    </div>
+    <div class="section-main-2">
+        <div class="talent">
+            <h3>Talent</h3>
+            <div class="talent-content">
+                <div class="container-artists">
+                    <div class="art-by"> Art by:</div>
+                    <div class="artists">
+                        <span class="artist">
+                            @foreach ($comics['artists'] as $artist)
+                                {{ $artist }},
+                            @endforeach
+                        </span>
+                    </div>
+                </div>
+
+                <div class="container-writers">
+                    <div class="written-by"> Written-by:</div>
+                    <div class="writers">
+                        <span class="writers">
+                            @foreach ($comics['writers'] as $writers)
+                                {{ $writers }},
+                            @endforeach
+                        </span>
+                    </div>
+                </div>
+
+            </div>
+
+
+        </div>
+        <div class="specs">
+            <h3>Specs</h3>
+
+            <div class="series">
+                <p>
+                    Series:
+                </p>
+                <span class="blue">{{ $comics['series'] }}</span>
+
+            </div>
+            <div class="usprice">
+                <p>
+                    U.S Price:
+                </p>
+                {{ $comics['price'] }}
+            </div>
+
+            <div class="date">
+                <p>On Sale Date:</p>
+                {{ $comics['sale_date'] }}
+            </div>
         </div>
     </div>
 @endsection
